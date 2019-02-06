0d59b4c25563c361ecd8fb10ed1aa84a27b836bd
amir saleem 1990 <31971908+amirsaleem1990@users.noreply.github.com>
2019-02-06 16:05:07 +0500

    updated

--- git-updates.py
+++ git-updates.py
@@ -13,6 +13,14 @@ for repo in repos:
         os.system("git status > /home/amir/Desktop/temprory-github-folder/{}.txt".format(repo))
         a = open("/home/amir/Desktop/temprory-github-folder/{}.txt".format(repo), 'r').read()
         if not "nothing to commit, working tree clean" in a:
-            print(repo)
+            aa = [i.strip() for i in a.splitlines()]
+            print("**********" , repo , "**********")
+            for i in aa:
+                if i :
+                    if i.startswith("On branch") or i.startswith("Your branch")  or i.startswith("Changes not staged") or i.startswith("(") or i.startswith("no changes added to") or i.startswith("Untracked files") or i.startswith("nothing added to commit"):
+                        pass
+                    else:
+                        print(i)
+            print("\n\n")
         os.chdir("..")
 os.system("rm -r /home/amir/Desktop/temprory-github-folder")

eaba9578066277b04f466052b9a3fdfd307c881f
amir saleem 1990 <amirsaleem1990@hotmail.com>
2019-12-17 12:28:51 +0500

    Create git_notes.sh

---
+++ git_notes.sh
@@ -0,0 +1,3 @@
+# ksi particuler commit wala data par apni repo ko wapis ly jany k lye.
+# eg: mujhy apni <Linux> repo ko ca02af2 waly commit k bad wali state par ly jana h or is commit k bad jitny bhi commit hwy un sab ko undo karna h to:
+cd github/Linux ; git checkout master; git reset --hard ca02af2; git push origin +master
